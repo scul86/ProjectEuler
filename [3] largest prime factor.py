229b0cfa4732575661361bf96db6cde23695f02e
scul86 <kyle.sims@comcast.net>
2017-04-22 14:12:17 -0600

    challenge 3 complete

--- [3] largest prime factor.py
+++ [3] largest prime factor.py
@@ -1,2 +1,17 @@
 
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
 
+primes = prime_factors(600851475143)
+
+print(primes)
