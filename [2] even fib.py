70bffb8b7046f23deda1554e970a80e775be4677
scul86 <kyle.sims@comcast.net>
2017-04-28 21:12:58 -0600

    updated challenge 2 to use formulas package

--- [2] even fib.py
+++ [2] even fib.py
@@ -1,17 +1,7 @@
-from functools import lru_cache as memoize
+from formulas import fibonacci
 
 
-@memoize()
-def fib(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fib(n-1) + fib(n-2)
-
-
-fib_list = [fib(n) for n in range(50)]
+fib_list = [fibonacci(n) for n in range(50)]
 
 fib_trimmed = [n for n in fib_list if not n%2 and n < 4000000]
 
